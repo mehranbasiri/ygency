36bb3dfa584c78ce1bec1266077504c33bb609ec
Mehran Basiri <Mehranbasirideveloper@gmail.com>
2024-01-18 23:17:58 +0330

    complete 3 container from homePage

--- src/HomePage/Web_development.jsx
+++ src/HomePage/Web_development.jsx
@@ -5,7 +5,30 @@ import arrowImg from "../images/arrow.png";
 import avatar from "../images/avatar.png";
 import halfCircle from "../images/half-ellipse.png";
 import heroImg from "../images/hero-two-min.jpg";
+import { IoIosArrowRoundUp } from "react-icons/io";
+import { skills } from "./data";
+import banner_hero from "../images/add-banner-min.png";
+import { useAnimation, motion, useInView } from "framer-motion";
+import { LiaArrowRightSolid } from "react-icons/lia";
+import CountUp from "react-countup";
+import aboutShape from "../images/about-image-shape-min.png";
 const Web_development = () => {
+  const AnimRight = {
+    hidden: {
+      opacity: 0,
+      x: +75,
+      transition: { ease: "easeOut", duration: 0.75 },
+    },
+    visible: { opacity: 1, x: 0 },
+  };
+  const titleAnimTop = {
+    hidden: {
+      opacity: 0,
+      y: 75,
+      transition: { ease: "easeOut", duration: 0.75 },
+    },
+    visible: { opacity: 1, y: 0 },
+  };
   return (
     <div className="web-development">
       <div className="top-title">
@@ -25,29 +48,187 @@ const Web_development = () => {
         <h1 className="top-titlle-h1">Develop Agency</h1>
 
         <div className="second-top-con">
-          <div >
+          <div className="first-div">
             <p>
               Sed ut perspiciatis unde omnis iste natus error voluptatem
               accusantium doloremque laudantium totam reme aperiam eaque abillo
               inventore
             </p>
             <img src={arrowImg} alt="arrow-img" />
-            <div>
+            <div className="popular-clients">
               <img src={avatar} alt="avatar-img" />
               <span>100+ Popular Clients</span>
             </div>
           </div>
-         
-            <img src={heroImg} alt="hero-img" className="hero-img" />
-        
+
+          <img src={heroImg} alt="hero-img" className="hero-img" />
+
           <div>
             <div className="explore-more-button">
-              <span>Explore More</span>
+              <IoIosArrowRoundUp />
+
+              <span>
+                Explore
+                <br /> More
+              </span>
             </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <div className="numerous-topics-con">
+        <div className="popular-project">
+          <motion.div
+            className="left"
+            variants={{
+              hidden: {
+                opacity: 0,
+                x: -75,
+                transition: { ease: "easeOut", duration: 0.75 },
+              },
+              visible: { opacity: 1, x: 0 },
+            }}
+            initial="hidden"
+            animate="visible"
+            transition={{
+              ease: "linear",
+              duration: 1,
+            }}
+          >
+            <img src={banner_hero} alt="banner-hero" className="left-hero" />
+            <div className="left-info">
+              <h1>150+</h1>
+              <h5>Project Completed</h5>
+              <hr />
+              <p>Quis autem vel eum reprehe voluptate velit esse quam</p>
+            </div>
+          </motion.div>
+          <motion.div
+            className="right"
+            variants={AnimRight}
+            initial="hidden"
+            transition={{
+              ease: "linear",
+              duration: 1,
+            }}
+            animate="visible"
+          >
+            <h3>
+              Innovative Web <br /> Development Solutions
+            </h3>
+            <hr />
+            <div className="info">
+              <img src={avatar} alt="avatar" />
+              <p>We denounce indignatione dislike mende charms</p>
+            </div>
+          </motion.div>
+        </div>
+        <div className="skills">
+          <motion.h1
+            variants={titleAnimTop}
+            initial="hidden"
+            animate="visible"
+            transition={{
+              ease: "linear",
+              duration: 1,
+            }}
+          >
+            Here are Numerous Topics That Will Enhance Your Skills
+          </motion.h1>
+          <div className="skills-items">
+            {skills.map((item) => (
+              <div className="skills-item" key={item.id}>
+                <img src={item.img} alt={item.title} />
+                <h2>{item.title}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="about-company">
+        <div className="about-company-text">
+          <motion.div variants={{
+             hidden: {
+              opacity: 0,
+              y: -75,
+              transition: { ease: "easeOut", duration: 0.75 },
+            },
+            visible: { opacity: 1, y: 0 },
+          }}
+            initial="hidden"
+            animate="visible"
+            transition={{
+              ease: "linear",
+              duration: 1,
+            }} >
+          <h4>
+            <LiaArrowRightSolid />
+            About Company
+          </h4>
+            <h1>We’re Award-winning Web Development Agency</h1>
+          
+          </motion.div>
+          <motion.div   variants={titleAnimTop}
+            initial="hidden"
+            animate="visible"
+            transition={{
+              ease: "linear",
+              duration: 1,
+            }}>
+          <div className="about-company-info">
+            <hr />
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptat emey
+              accusantium doloremque laudantium totam aperiam eaque quabillo
+              inventore veritatisey quasi architecto beatae dictasunt
+            </p>
+          </div>
+          
+          <div
+            className="about-compan-counters"
+          
+          >
+            <div className="counter-item">
+              <h2>
+                <CountUp duration={5} className="counter" end={100} />%
+              </h2>
+              <span>Clients Satisfactions</span>
+            </div>
+            <div className="counter-item">
+              <h2>
+                {" "}
+                <CountUp duration={5} className="counter" end={93} />%
+              </h2>
+              <span>Success Rating</span>
+            </div>{" "}
+            <div className="counter-item">
+              <h2>
+                {" "}
+                <CountUp duration={5} className="counter" end={55} />%
+              </h2>
+              <span>Project Complete</span>
+              </div>
+              
+            
+            </div>
+            </motion.div>
+          </div>
+        <div className="about-company-img">
+          <motion.img
+            src={aboutShape}
+            alt="about-shape"
+            variants={AnimRight}
+            initial="hidden"
+            transition={{
+              ease: "linear",
+              duration: 1,
+            }}
+            animate="visible"
+          />
+        </div>
+      
+    
+      </div></div>
   );
 };
 
---
+++ src/HomePage/data.js
@@ -0,0 +1,81 @@
+import  bootstrap from "../images/skill1.png";
+import html from "../images/skill2.png";
+import css from "../images/skill3.png";
+import javascript from "../images/skill4.png";
+import react from "../images/skill5.png";
+import wordPress from "../images/skill6.png";
+import php from "../images/skill7.png";
+import nodejs from "../images/skill7.png";
+import sass from "../images/skill8.png";
+import angular from "../images/skill9.png";
+import shopify from "../images/skill10.png";
+import elementor from "../images/skill11.png";
+import vuejs from "../images/skill12.png";
+
+export const skills = [
+  {
+    id: 0,
+    title: "Bootstrap",
+    img: [bootstrap ],
+  },
+  {
+    id: 1,
+    title: "HTML",
+    img: [html ],
+  },
+  {
+    id: 2,
+    title: "CSS",
+    img: [css ],
+  },
+  {
+    id: 3,
+    title: "javascript",
+    img: [javascript ],
+  },
+  {
+    id: 4,
+    title: "React",
+    img: [react ],
+  },
+  {
+    id: 5,
+    title: "Word{ress",
+    img: [wordPress ],
+  },
+  {
+    id: 6,
+    title: "php",
+    img: [php ],
+  },
+  {
+    id: 7,
+    title: "node.js",
+    img: [nodejs ],
+  },
+  {
+    id: 8,
+    title: "Sass",
+    img: [sass ],
+  },
+  {
+    id: 9,
+    title: "Angular",
+    img: [angular ],
+  },
+  {
+    id: 10,
+    title: "Shopify",
+    img: [shopify ],
+  },
+  {
+    id: 11,
+    title: "Elementor",
+    img: [elementor ],
+  },
+  {
+    id: 12,
+    title: "Vue.js",
+    img: [vuejs ],
+  },
+];
